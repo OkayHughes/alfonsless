9eb83aa6e59c25c22c099befed0fddc38e925a39
Owen Hughes <okayhughes@gmail.com>
2018-08-06 19:43:44 -0400

    Current iteration uses all constraints, is infeasible.

--- SOS_on_a_subset_okay.m
+++ SOS_on_a_subset_okay.m
@@ -10,10 +10,12 @@
 degree = 12 ;
 c = 0 ;
 
+intParams = FeketeCube(2,degree) ;
+
 %% SPOTLESS PROBLEM
 % variables
-x = msspoly('x',1) ;
-y = msspoly('y',1) ;
+x = intParams.mon_basis.variables(1) ;
+y = intParams.mon_basis.variables(2) ;
 
 % create program
 prog = spotsosprog ;
@@ -36,8 +38,6 @@ prog = sosOnK(prog,xcon-1,y,hY,degree) ;
 % create cost function
 int_XY = boxMoments([x;y],[-1;-1],[1;1]) ;
 obj = int_XY(mon)'*fcoeff ;
-int_XY
-assert 0;
 
 % run solver
 options = spot_sdp_default_options();
@@ -49,12 +49,12 @@ fspotless = sol.eval(fcoeff)'*mon ;
 
 %% ALFONSO PROBLEM
 % create interpolation points
-intParams = FeketeCube(2,degree/2) ;
+
 n   = intParams.n;
 d   = intParams.d;
 U   = intParams.U;
 L   = intParams.L;
-P   = intParams.P;
+P   = intParams.P0;
 pts = intParams.pts;
 
 % dimension of the weight polynomial space (should be dimension of d)
@@ -64,15 +64,14 @@ LWts = repmat(nchoosek(n+d-1,n),n,1);
 gH_Params.n = n;
 gH_Params.d = d;
 gH_Params.U = U;
-gH_Params.numPolys = 2;
-
+gH_Params.numPolys = 3;
 gH_Params.L     = L;
 gH_Params.LWts  = LWts;
-nu              = 2*(L+sum(LWts));
+nu              = gH_Params.numPolys*(L+sum(LWts)) ;
 gH_Params.bnu	= nu+1;
 gH_Params.P = P;
 
-% create polynomial hT (g in the alfonso paper) to define space T = [-1,1]
+% create polynomial hT (g in the alfonso paper) to define space T = [-1,1]^2
 wtVals = 1-pts.^2;
 PWts = cell(n,1);
 for j = 1:n
@@ -85,21 +84,26 @@ end
 gH_Params.PWts = PWts;
 
 % find the points where x = c
-plog = abs(pts(:,1) - c) < 1e-6 ;
-cpval = -10*ones(U,1) ;
-cpval(plog) = 1 ;
+% plog = abs(pts(:,1) - c) < 1e-6 ;
+% cpval = -10*ones(U,1) ;
+% cpval(plog) = 1 ;
 
 % create A,b,c matrices to define conic problem
-probData.A = sparse(repmat(eye(U),1,2)) ;
-probData.b = intParams.w ;
-probData.c = -[-ones(U,1) ; cpval] ;
-
+% probData.A = sparse(repmat(eye(U),1,2)) ;
+% probData.b = intParams.w ;
+% probData.c = -[-ones(U,1) ; cpval] ;
+application_matrix = vector_partial_application(1, c, intParams.mon_basis);
+probData.A = sparse([eye(U), -1 * eye(U), zeros(U);
+                     intParams.mon_to_P0 * application_matrix * intParams.P0_to_mon, zeros(U), -eye(U)]);
+probData.b = [intParams.mon_to_P0 * msspoly_to_vector(msspoly(-1), intParams.mon_basis);
+              intParams.mon_to_P0 * msspoly_to_vector(msspoly(1), intParams.mon_basis)];
+probData.c = [intParams.w; zeros(intParams.U * 2, 1)];
 % make initial primal iterate
-x0 = ones(2*U,1) ;
+x0 = ones(gH_Params.numPolys*U,1) ;
 [~, g0, ~, ~] = alfonso_grad_and_hess(x0, gH_Params);
 rP = max((1+abs(probData.b))./(1+abs(probData.A*x0)));
 rD = max((1+abs(g0))./(1+abs(probData.c)));
-x0 = repmat(sqrt(rP*rD),2*U,1);  
+x0 = repmat(sqrt(rP*rD),gH_Params.numPolys*U,1);  
 
 % run alfonso
 opts.optimTol = 1e-6 ;
@@ -107,10 +111,12 @@ results = alfonso(probData, x0, @alfonso_grad_and_hess, gH_Params, opts);
 
 %% RECOVER ALFONSO SOLUTION AS MSSPOLY
 % recover solution
-zvals = -results.y ;
-data = msubs(mon,[x;y],pts') ;
-coeff = data'\zvals ;
-falfonso = coeff'*mon ;
+%zvals = -results.y ;
+%data = msubs(mon,[x;y],pts') ;
+%coeff = data'\zvals ;
+%falfonso = coeff'*mon ;
+
+falfonso = (intParams.P0_to_mon * results.x(1:U, :))' * intParams.mon_basis.monomials;
 
 %% COMPARE SOLUTIONS
 fvals_spotless = msubs(fspotless,[x;y],pts') ;
@@ -121,7 +127,7 @@ fvals_spotless = msubs(fspotless,[x;y],pts') ;
 
 fvals_alfonso =  msubs(falfonso,[x;y],pts') ;
 
-max(abs(fvals_spotless(:) - fvals_alfonso(:)))
+%max(abs(fvals_spotless(:) - fvals_alfonso(:)))
 
 %% PLOTTING POLYNOMIAL (RECOVERED) OUTPUT
 % spotless setup
