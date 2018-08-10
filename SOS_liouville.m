25eea4dda2ce5c60ee01adee0814a65ca81d2f28
Owen Hughes <okayhughes@gmail.com>
2018-08-10 15:25:55 -0400

    Create most important test

--- SOS_liouville.m
+++ SOS_liouville.m
@@ -7,11 +7,19 @@
 %           f(c,.)  >= +1   for all y \in Y
 %
 %% user parameters 
-degree = 12 ;
+degree = 8 ;
 c = 0 ;
+t = msspoly('t');
+x = msspoly('x');
+variables = [t;x];
+f = variables(2)^2;
+T = 1;
+intParams_w = FeketeCube(size(x, 1), degree/2,x) ;
 
-intParams = FeketeCube(2,degree/2) ;
+intParams_v_f = FeketeCube(size(variables, 1), degree/2 + ceil(msspoly_degree(f)/2), variables); 
+intParams_arr = [intParams_v_f, intParams_w, intParams_w, intParams_w];
 
+intParams_v = FeketeCube(size(variables, 1), degree/2, variables);
 % %% SPOTLESS PROBLEM
 % % variables
 % x = intParams.mon_basis.variables(1) ;
@@ -51,38 +59,94 @@ intParams = FeketeCube(2,degree/2) ;
 %% ALFONSO PROBLEM
 % create interpolation points
 
-n   = intParams.n;
-d   = intParams.d;
-U   = intParams.U;
-L   = intParams.L;
-P   = intParams.P0;
-pts = intParams.pts;
+numPolys = 4;
+gH_Params.numPolys = numPolys;
+gH_Params.n_arr = zeros(numPolys, 1);
+gH_Params.d_arr = zeros(numPolys, 1);
+gH_Params.U_arr = zeros(numPolys, 1);
+gH_Params.L_arr = zeros(numPolys, 1);
+gH_Params.LWts_cell = cell(numPolys, 1);
+gH_Params.P_cell = cell(numPolys, 1);
+gH_Params.PWts_cell = cell(numPolys, 1);
+gH_Params.bnu = 0;
 
-% dimension of the weight polynomial space (should be dimension of d)
-LWts = repmat(nchoosek(n+d-1,n),n,1);
+for i=1:numPolys
+    intParams_i = intParams_arr(i);
+    n   = intParams_i.n;
+    d   = intParams_i.d;
+    U   = intParams_i.U;
+    L   = intParams_i.L;
+    P   = intParams_i.P0;
+    pts = intParams_i.pts;
 
-% parameter object for cone gradient/hessian function
-gH_Params.n = n;
-gH_Params.d = d;
-gH_Params.U = U;
-gH_Params.numPolys = 2;
-gH_Params.L     = L;
-gH_Params.LWts  = LWts;
-nu              = gH_Params.numPolys*(L+sum(LWts));
-gH_Params.bnu   = nu+1;
-gH_Params.P = P;
+    % dimension of the weight polynomial space (should be dimension of d)
+    LWts = repmat(nchoosek(n+d-1,n),n,1);
 
-% create polynomial hT (g in the alfonso paper) to define space T = [-1,1]^2
-wtVals = 1-pts.^2;
-PWts = cell(n,1);
-for j = 1:n
-    PWts{j}         = diag(sqrt(wtVals(:,j)))*P(:,1:LWts(j));
-    [PWts{j}, ~]    = qr(PWts{j}, 0);
-    % associated positive semidefinite cone constraints: 
-    % PWts{j}'*diag(x_1)*PWts{j} >= 0,
-    % PWts{j}'*diag(x_2)*PWts{j} >= 0,...
+    % parameter object for cone gradient/hessian function
+    gH_Params.n_arr(i)      = n;
+    gH_Params.d_arr(i)      = d;
+    gH_Params.U_arr(i)      = U;
+    gH_Params.L_arr(i)      = L;
+    gH_Params.LWts_cell{i}  = LWts;
+    nu                      = L+sum(LWts) ;
+    gH_Params.bnu           = gH_Params.bnu + nu;
+    gH_Params.P_cell{i}     = P;
+
+
+    % create polynomial hT (g in the alfonso paper) to define space T = [-1,1]^2
+    wtVals = 1-pts.^2;
+    PWts = cell(n,1);
+    for j = 1:n
+        PWts{j}         = diag(sqrt(wtVals(:,j)))*P(:,1:LWts(j));
+        [PWts{j}, ~]    = qr(PWts{j}, 0);
+        % associated positive semidefinite cone constraints: 
+        % PWts{j}'*diag(x_1)*PWts{j} >= 0,
+        % PWts{j}'*diag(x_2)*PWts{j} >= 0,...
+    end
+    gH_Params.PWts_cell{i} = PWts;
 end
-gH_Params.PWts = PWts;
+gH_Params.bnu = gH_Params.bnu + 1;
+
+v_to_v_f = monomial_to_monomial(intParams_v.mon_basis, intParams_v_f.mon_basis);
+time_der_v = vector_derivative(1, intParams_v.mon_basis);
+mult_mat = vector_poly_multiply(f, intParams_v.mon_basis, intParams_v_f.mon_basis);
+space_der_v = vector_derivative(2, intParams_v.mon_basis);
+
+A1 = intParams_v_f.mon_to_P0 * (v_to_v_f * time_der_v + mult_mat * space_der_v) * intParams_v.P0_to_mon;
+A2 = zeros(intParams_v_f.U, intParams_w.U);
+
+v_to_w = monomial_to_monomial(intParams_v.mon_basis, intParams_w.mon_basis);
+apply_final_time = vector_partial_application(1, T, intParams_v.mon_basis);
+
+B1 = intParams_w.mon_to_P0 * v_to_w * apply_final_time * intParams_v.P0_to_mon;
+B2 = zeros(intParams_w.U);
+
+apply_init_time = vector_partial_application(1, -1, intParams_v.mon_basis);
+
+C1 = intParams_w.mon_to_P0 * v_to_w * apply_init_time * intParams_v.P0_to_mon;
+C2 = eye(intParams_w.U);
+
+D1 = zeros(intParams_w.U, intParams_v.U);
+D2 = eye(intParams_w.U);
+
+const1 = zeros(intParams_v_f.U, 1);
+const2 = zeros(intParams_w.U, 1);
+const3 = ones(intParams_w.U, 1);
+const4 = zeros(intParams_w.U, 1);
+
+int1 = zeros(intParams_v.U, 1);
+int2 = intParams_w.w;
+
+A = -[A1, A2;
+     B1, B2;
+     C1, C2;
+     D1, D2]';
+c = -[const1;
+      const2;
+      const3;
+      const4];
+b = -[int1;
+      int2];
 
 % find the points where x = c
 % plog = abs(pts(:,1) - c) < 1e-6 ;
@@ -93,12 +157,6 @@ gH_Params.PWts = PWts;
 % probData.A = sparse(repmat(eye(U),1,2)) ;
 % probData.b = intParams.w ;
 % probData.c = -[-ones(U,1) ; cpval] ;
-derivative_matrix = vector_derivative(1, intParams.mon_basis);
-probData.A = sparse([-eye(intParams.U); -intParams.mon_to_P0 * derivative_matrix * intParams.P0_to_mon]');
-mon_con1 = msspoly(1);
-mon_con1_vec = intParams.mon_to_P0 * msspoly_to_vector(mon_con1, intParams.mon_basis);
-probData.c = [zeros(intParams.U, 1); mon_con1_vec];
-probData.b = -[intParams.w];
 % make initial primal iterate
 x0 = ones(gH_Params.numPolys*U, 1);
 [~, g0, ~, ~] = alfonso_grad_and_hess(x0, gH_Params);
