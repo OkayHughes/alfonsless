9c61bacb217fe8f792607d201179a2538caa1181
Owen Hughes <okayhughes@gmail.com>
2018-08-10 21:16:15 -0400

    Minor adjustments to deal with affine rectangular domains

--- okay_polynomial_utilities/monomial_to_interpolant.m
+++ okay_polynomial_utilities/monomial_to_interpolant.m
@@ -21,16 +21,19 @@ function [P0_to_mon, P_to_mon, mon_to_P0, mon_to_P] = monomial_to_interpolant(in
     for mon_ind=1:size(mss_chebyshev, 1)
         M(:, mon_ind) = msspoly_to_vector(mss_chebyshev(mon_ind), basis);
     end
-
-    cond_num = cond(M);
+    'mon'
+    cond_num = cond(full(M))
 
     if cond_num > 1E5
         warning("Condition number of polynomial basis is large (cond(M) = %d)\nCOB matrices may be inaccurate.", cond_num);
     end
-    cond_num = cond(interpolant_basis);
+    'interp'
+    cond_num = cond(full(interpolant_basis))
     if cond_num > 1E5
         warning("Condition number of P0 is large (cond(P0) = %d)\nCOB matrices may be inaccurate.", cond_num);
     end
+
+
     P0_to_mon = M * inv(interpolant_basis);
     P_to_mon = M * inv(interpolant_basis_orth);
     M_inv = inv(M);
