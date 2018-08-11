b476f95fbc4f6b7c928a582d5f1c276e1dc1e0b2
Owen Hughes <okayhughes@gmail.com>
2018-08-10 21:15:48 -0400

    Test finally passing. MATLAB IS FUCKIN TRASH, MY DUDE

---
+++ tests/test_scaled_fekete_cube.m
@@ -0,0 +1,14 @@
+cube = FeketeCube(2, 4);
+err = 0;
+sc = scale_fekete_cube(cube, [-2, 3; -2, 2]);
+for i=1:size(sc.P0_full, 2)
+    f = (sc.P0_to_mon * sc.P0_full(:, i))' * sc.mon_basis.monomials;
+    tmp_vec = dmsubs(f, sc.mon_basis.variables, sc.pts');
+    err = err + norm(tmp_vec' - sc.P0_full(:, i));
+end
+
+if err > 1
+    error("scaled COB matrix is hot trash")
+elseif err > 1E-6
+    warning("Scaled COB matrix may be inaccurate")
+end
