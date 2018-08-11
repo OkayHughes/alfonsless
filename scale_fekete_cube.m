6707a49fa0e05445b96313f141336ea8c1b10f28
Owen Hughes <okayhughes@gmail.com>
2018-08-10 21:16:44 -0400

    Finally works correctly. scale_fekete_cube should offset the interpolant basis.

--- scale_fekete_cube.m
+++ scale_fekete_cube.m
@@ -13,8 +13,8 @@ function scaled_cube = scale_fekete_cube(cube, bounds)
     scaled_cube.polynomials = subs(scaled_cube.polynomials, ...
                                    scaled_cube.mon_basis.variables, ...
                                    change_of_variables);
-    %size(dmsubs(scaled_cube.polynomials, scaled_cube.mon_basis.variables, scaled_cube.pts')')
-    %size()
+
+
     if norm(dmsubs(scaled_cube.polynomials, scaled_cube.mon_basis.variables, scaled_cube.pts')' - scaled_cube.P0_full) > 1E-5
         warning("something is wrong with polynomial composition")
     end
@@ -23,8 +23,8 @@ function scaled_cube = scale_fekete_cube(cube, bounds)
     
     [P0_to_mon, P_to_mon, mon_to_P0, mon_to_P] = monomial_to_interpolant(scaled_cube.P0_full, scaled_cube.P_full, scaled_cube.polynomials, scaled_cube.mon_basis);
     
-    intParams.P0_to_mon = P0_to_mon;
-    intParams.P_to_mon = P_to_mon;
-    intParams.mon_to_P0 = mon_to_P0;
-    intParams.mon_to_P = mon_to_P;
+    scaled_cube.P0_to_mon = P0_to_mon;
+    scaled_cube.P_to_mon = P_to_mon;
+    scaled_cube.mon_to_P0 = mon_to_P0;
+    scaled_cube.mon_to_P = mon_to_P;
 end
