99d4ca3566c4f0d06f96ecdcb079e6bfe4723967
Owen Hughes <okayhughes@gmail.com>
2018-08-12 22:18:23 -0400

    Renamed polynomial_evaluation to shreyas_polynomial_utils

--- polynomial_evaluation/eval_w.m
+++
@@ -1,7 +0,0 @@
-function w_out = eval_w(k,wcoeffs,wpowers)
-% w_out = eval_w(k,wcoeffs,wpowers)
-%
-% Evaluate a polynomial w(k), expressed as a coefficient matrix and a
-% powers matrix, at the point k (2-by-1)
-    w_out = wcoeffs*prod(repmat(k,1,size(wpowers,1))'.^wpowers,2) ;
-end
