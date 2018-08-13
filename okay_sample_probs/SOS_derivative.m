9478e3a9fbf420f24d0cc999bda64e354cb6148e
Owen Hughes <okayhughes@gmail.com>
2018-08-13 14:34:47 -0400

    So this works, try something else asshole.

--- okay_sample_probs/SOS_derivative.m
+++ okay_sample_probs/SOS_derivative.m
@@ -7,158 +7,151 @@
 %           f(c,.)  >= +1   for all y \in Y
 %
 %% user parameters 
-degree = 12 ;
-c = 0 ;
 
-intParams = FeketeCube(2,degree/2) ;
-
-% %% SPOTLESS PROBLEM
-% % variables
-% x = intParams.mon_basis.variables(1) ;
-% y = intParams.mon_basis.variables(2) ;
-
-% % create program
-% prog = spotsosprog ;
-% prog = prog.withIndeterminate(x) ;
-% prog = prog.withIndeterminate(y) ;
-
-% % bounds on variables
-% hX = (x + 1).*(1 - x) ;
-% hY = (y + 1).*(1 - y) ;
-
-% % create decision variable
-% mon = monomials([x;y],0:degree) ;
-% [prog,f,fcoeff] = prog.newFreePoly(mon) ;
-
-% % create SOS constraints
-% xcon = msubs(f,x,c) ;
-% prog = sosOnK(prog,f+1,[x;y],[hX;hY],degree) ;
-% prog = sosOnK(prog,xcon-1,y,hY,degree) ;
-% prog = sosOnK(prog, f, [x;y], [hX; hY], degree)
-
-% % create cost function
-% int_XY = boxMoments([x;y],[-1;-1],[1;1]) ;
-% obj = int_XY(mon)'*fcoeff ;
-
-% % run solver
-% options = spot_sdp_default_options();
-% options.verbose = 1 ;
-% sol = prog.minimize(obj, @spot_mosek, options);
-
-% % recover f
-% fspotless = sol.eval(fcoeff)'*mon ;
 
 %% ALFONSO PROBLEM
 % create interpolation points
+function SOS_derivative()
 
-n   = intParams.n;
-d   = intParams.d;
-U   = intParams.U;
-L   = intParams.L;
-P   = intParams.P0;
-pts = intParams.pts;
+variables = msspoly('x');
+f=variables(1)^3+3;
+degree = 10;
+S_bounds = [-1, 2];
+intParams_w = FeketeCube(size(variables, 1), degree/2, variables);
+intParams_w = scale_fekete_cube(intParams_w, S_bounds);
 
-% dimension of the weight polynomial space (should be dimension of d)
-LWts = repmat(nchoosek(n+d-1,n),n,1);
 
-% parameter object for cone gradient/hessian function
-gH_Params.n = n;
-gH_Params.d = d;
-gH_Params.U = U;
-gH_Params.numPolys = 2;
-gH_Params.L     = L;
-gH_Params.LWts  = LWts;
-nu              = gH_Params.numPolys*(L+sum(LWts));
-gH_Params.bnu   = nu+1;
-gH_Params.P = P;
+intParams_w_f = FeketeCube(size(variables, 1), degree/2 + ceil(msspoly_degree(f)/2), variables); 
+intParams_w_f = scale_fekete_cube(intParams_w_f, S_bounds);
 
-% create polynomial hT (g in the alfonso paper) to define space T = [-1,1]^2
-wtVals = 1-pts.^2;
-PWts = cell(n,1);
-for j = 1:n
-    PWts{j}         = diag(sqrt(wtVals(:,j)))*P(:,1:LWts(j));
-    [PWts{j}, ~]    = qr(PWts{j}, 0);
-    % associated positive semidefinite cone constraints: 
-    % PWts{j}'*diag(x_1)*PWts{j} >= 0,
-    % PWts{j}'*diag(x_2)*PWts{j} >= 0,...
+intParams_arr = [intParams_w, intParams_w_f];
+
+numPolys = 2;
+
+gH_Params.numPolys = numPolys;
+gH_Params.n_arr = zeros(numPolys, 1);
+gH_Params.d_arr = zeros(numPolys, 1);
+gH_Params.U_arr = zeros(numPolys, 1);
+gH_Params.L_arr = zeros(numPolys, 1);
+gH_Params.LWts_cell = cell(numPolys, 1);
+gH_Params.P_cell = cell(numPolys, 1);
+gH_Params.PWts_cell = cell(numPolys, 1);
+gH_Params.bnu = 0;
+
+for i=1:numPolys
+    intParams_i = intParams_arr(i);
+    n   = intParams_i.n;
+    d   = intParams_i.d;
+    U   = intParams_i.U;
+    L   = intParams_i.L;
+    P   = intParams_i.P0;
+    pts = intParams_i.pts;
+    bounds = intParams_i.bounds;
+    lb = bounds(:, 1);
+    ub = bounds(:, 2);
+
+    % dimension of the weight polynomial space (should be dimension of d)
+    LWts = repmat(nchoosek(n+d-1,n),n,1);
+    % parameter object for cone gradient/hessian function
+    gH_Params.n_arr(i)      = n;
+    gH_Params.d_arr(i)      = d;
+    gH_Params.U_arr(i)      = U;
+    gH_Params.L_arr(i)      = L;
+    gH_Params.LWts_cell{i}  = LWts;
+    nu                      = L+sum(LWts) ;
+    gH_Params.bnu           = gH_Params.bnu + nu;
+    gH_Params.P_cell{i}     = P;
+
+    % create polynomial hT (g in the alfonso paper) to define space T = [-1,1]^2
+
+    wtVals  = bsxfun(@minus,pts,lb').*bsxfun(@minus,ub',pts);
+
+    PWts = cell(n,1);
+    for j = 1:n
+        PWts{j}         = diag(sqrt(wtVals(:,j)))*P(:,1:LWts(j));
+        [PWts{j}, ~]    = qr(PWts{j}, 0);
+        % associated positive semidefinite cone constraints: 
+        % PWts{j}'*diag(x_1)*PWts{j} >= 0,
+        % PWts{j}'*diag(x_2)*PWts{j} >= 0,...
+    end
+    gH_Params.PWts_cell{i} = PWts;
 end
-gH_Params.PWts = PWts;
+gH_Params.bnu = gH_Params.bnu + 1;
 
-% find the points where x = c
-% plog = abs(pts(:,1) - c) < 1e-6 ;
-% cpval = -10*ones(U,1) ;
-% cpval(plog) = 1 ;
+
 
 % create A,b,c matrices to define conic problem
 % probData.A = sparse(repmat(eye(U),1,2)) ;
 % probData.b = intParams.w ;
 % probData.c = -[-ones(U,1) ; cpval] ;
-derivative_matrix = vector_derivative(1, intParams.mon_basis);
-probData.A = sparse([-eye(intParams.U); -intParams.mon_to_P0 * derivative_matrix * intParams.P0_to_mon]');
-mon_con1 = msspoly(1);
-mon_con1_vec = intParams.mon_to_P0 * msspoly_to_vector(mon_con1, intParams.mon_basis);
-probData.c = [zeros(intParams.U, 1); mon_con1_vec];
-probData.b = -[intParams.w];
+
+A1 = eye(intParams_w.U);
+
+mult_mat = vector_poly_multiply(f, intParams_w.mon_basis, intParams_w_f.mon_basis);
+time_der = vector_derivative(1, intParams_w.mon_basis);
+
+B1 = intParams_w_f.mon_to_P0 * mult_mat * time_der * intParams_w.P0_to_mon;
+
+const1 = zeros(intParams_w.U, 1);
+const2 = ones(intParams_w_f.U, 1);
+
+int1 = intParams_w.w;
+
+probData.A = -[A1;
+              B1]';
+
+probData.c = -[const1;  
+              const2];
+probData.b = -[int1];
 % make initial primal iterate
-x0 = ones(gH_Params.numPolys*U, 1);
+x0 = ones(sum(gH_Params.U_arr), 1);
 [~, g0, ~, ~] = alfonso_grad_and_hess(x0, gH_Params);
 rP = max((1+abs(probData.b))./(1+abs(probData.A*x0)));
 rD = max((1+abs(g0))./(1+abs(probData.c)));
-x0 = repmat(sqrt(rP*rD),gH_Params.numPolys*U,1);  
+x0 = repmat(sqrt(rP*rD),sum(gH_Params.U_arr),1);  
 
 % run alfonso
 opts.optimTol = 1e-6 ;
 results = alfonso(probData, x0, @alfonso_grad_and_hess, gH_Params, opts);
 
-%% RECOVER ALFONSO SOLUTION AS MSSPOLY
-% recover solution
-%zvals = -results.y ;
-%data = msubs(mon,[x;y],pts') ;
-%coeff = data'\zvals ;
-%falfonso = coeff'*mon ;
+falfonso = (intParams_w.P0_to_mon * results.y)' * intParams_w.mon_basis.monomials;
 
-falfonso = (intParams.P0_to_mon * results.x(1:U, :))' * intParams.mon_basis.monomials;
+%run spotless problem
+hS = -(variables-S_bounds(:, 1)).*(variables-S_bounds(:, 2));
+dl=boxMoments(variables, S_bounds(:, 1), S_bounds(:, 2));
+prog = spotsosprog;
+prog = prog.withIndeterminate(variables);
+wmonom = monomials(variables, 0:degree) ;
+[prog, w, wcoeff] = prog.newFreePoly(wmonom) ;
+bad_operator = diff(w, variables) * f;
+prog = sosOnK(prog, bad_operator-1, variables, hS, degree) ;
+prog = sosOnK(prog, w, variables, hS, degree);
+obj = dl(wmonom)' * wcoeff ;
 
-%% COMPARE SOLUTIONS
-fvals_spotless = msubs(fspotless,[x;y],pts') ;
+options = spot_sdp_default_options();
+options.verbose = 1;
+sol = prog.minimize(obj, @spot_mosek, options);
 
-% fvals_alfonso  = -results.y ; % the direct output from alfonso, should
-%                               % have nearly identical values to the line
-%                               % below
+fspotless = sol.eval(w) ;
 
-fvals_alfonso =  msubs(falfonso,[x;y],pts') ;
 
-%max(abs(fvals_spotless(:) - fvals_alfonso(:)))
 
-%% PLOTTING POLYNOMIAL (RECOVERED) OUTPUT
-% spotless setup
-[X,Y] = makeContourAxes() ;
-XY = makeContourAxes() ;
 
-% alfonso setup
-xvals = pts(:,1) ;
-yvals = pts(:,2) ;
-tri = delaunay(xvals,yvals);
 
 figure(1) ; cla ; hold on ;
+%xlim([-1, 2])
+%ylim([-1, 2])
+
+tvec = linspace(-1,2,500) ;
 
 % spotless output
-F1 = reshape(full(msubs(fspotless,[x;y],XY)),100,100) ;
-% surf(X,Y,F)
+fvals = dmsubs(fspotless,variables,tvec) ;
+plot(tvec,fvals,'--', 'LineWidth',1.5)
 
-% alfonso output
-F2 = reshape(full(msubs(falfonso,[x;y],XY)),100,100) ;
+% alfonso output:
+%yvals = -results.y ; % these are the values of the polynomial f at the points "pts"
 
-% plot error between spotless and alfonso on X*Y
-surf(X,Y,F1-F2)
+monvals = dmsubs(falfonso,variables,tvec);
+plot(tvec,monvals,'LineWidth',1);
 
-%% PLOTTING COARSE OUTPUT
-figure(2) ; cla ; hold on
-
-% spotless output
-zvals = full(fvals_spotless) ;
-trisurf(tri, xvals,yvals,zvals);
-
-% alfonso output
-zvals = -results.y ;
-trisurf(tri, xvals,yvals,zvals);
+end
