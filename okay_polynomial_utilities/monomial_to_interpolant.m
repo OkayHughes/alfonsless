735c19f2e0ff5f978b1b69d106ba848445a562cf
Owen Hughes <okayhughes@gmail.com>
2018-08-12 22:09:31 -0400

    Still working on interpolant support, and scaling.

--- okay_polynomial_utilities/monomial_to_interpolant.m
+++ okay_polynomial_utilities/monomial_to_interpolant.m
@@ -22,19 +22,18 @@ function [P0_to_mon, P_to_mon, mon_to_P0, mon_to_P] = monomial_to_interpolant(in
         M(:, mon_ind) = msspoly_to_vector(mss_chebyshev(mon_ind), basis);
     end
     cond_num = cond(full(M));
-
-    if cond_num > 1E5
+    M_inv = inv(M);
+    if cond_num > 1E10
         warning("Condition number of polynomial basis is large (cond(M) = %d)\nCOB matrices may be inaccurate.", cond_num);
     end
     cond_num = cond(full(interpolant_basis));
-    if cond_num > 1E5
+    if cond_num > 1E10
         warning("Condition number of P0 is large (cond(P0) = %d)\nCOB matrices may be inaccurate.", cond_num);
     end
 
 
     P0_to_mon = M * inv(interpolant_basis);
     P_to_mon = M * inv(interpolant_basis_orth);
-    M_inv = inv(M);
     mon_to_P = interpolant_basis_orth * M_inv;
     mon_to_P0 = interpolant_basis * M_inv;
 end
