1833b6f8aa48f933d955504cae6c5ca4fa03739a
Owen Hughes <okayhughes@gmail.com>
2018-08-14 00:14:07 -0400

    Finish first draft of find_FRS_alfonso.m

--- find_FRS_alfonso.m
+++ find_FRS_alfonso.m
@@ -1,17 +1,21 @@
 %% problem description
 %
 % TODO: Write problem 
-function find_FRS_alfonso(prob)
+function out = find_FRS_alfonso(prob)
 
 degree = prob.degree;
 
 t = prob.t;
 x = prob.x;
 k = prob.k;
+
 f = prob.f;
+deg_f = ceil(max(arrayfun(@msspoly_degree, f))/2) * 2;
+
 error_dynamics = isfield(prob, 'g');
 if error_dynamics
     g = prob.g;
+    deg_g = ceil(max(arrayfun(@msspoly_degree, g))/2) * 2;
 end
 
 X_bounds = prob.X_bounds;
@@ -23,189 +27,218 @@ T_max = prob.T;
 
 %we make the following definitions:
 % X = X_s
-% S = [0, T] x X x K
-% 
+% Y = X x K
+% Y0 = X0 x K
+% Z = [0, T] x X x K
 
+Y_vars = [x; k];
+Y_bounds = [X_bounds;
+            K_bounds];
 
-intParams_w = FeketeCube(size(x, 1), degree/2, x);
-intParams_w = scale_fekete_cube(intParams_w, X_bounds);
+Y0_bounds = [X0_bounds;
+            K_bounds];
 
-X_T_bounds = [0.8, 1]
+Z_vars = [t;x;k];
+Z_bounds = [[T_min, T_max];
+            X_bounds;
+            K_bounds];
 
-intParams_w_X_T = FeketeCube(size(x, 1), degree/2, x);
-intParams_w_X_T = scale_fekete_cube(intParams_w_X_T, X_T_bounds);
+int_params_Y = AffineFeketeCube(Y_vars, degree, Y_bounds);
+int_params_Y0 = AffineFeketeCube(Y_vars, degree, Y0_bounds);
 
-S_bounds = [[T_min, T_max];
-            X_bounds];
+int_params_Z = AffineFeketeCube(Z_vars, degree, Z_bounds);
+%Zf basis spans f_i*v
+int_params_Zf = AffineFeketeCube(Z_vars, degree + deg_f, Z_bounds);
+%Zf basis spans g_i*v
+if error_dynamics
+    int_params_Zg = AffineFeketeCube(Z_vars, degree + deg_g, Z_bounds);
+end
+if error_dynamics
+    int_params_arr = [int_params_Zf, int_params_Zg, int_params_Zg, ...
+                      int_params_Z, int_params_Y0, int_params_Y, int_params_Z];
+else
+    int_params_arr = [int_params_Zf, int_params_Y0, ...
+                      int_params_Y, int_params_Z];
+end
 
-intParams_v_f = FeketeCube(size(variables, 1), degree/2 + ceil(msspoly_degree(f)/2 + 4), variables); 
-intParams_v_f = scale_fekete_cube(intParams_v_f, S_bounds);
-intParams_arr = [intParams_v_f, intParams_w_X_T, intParams_w, intParams_w];
-
-
-intParams_v = FeketeCube(size(variables, 1), degree/2, variables);
-intParams_v = scale_fekete_cube(intParams_v, S_bounds);
-
+mon_basis_Y = int_params_Y.mon_basis;
+mon_basis_Y0 = int_params_Y0.mon_basis;
+mon_basis_Z = int_params_Z.mon_basis;
+mon_basis_Zf = int_params_Zf.mon_basis;
+mon_basis_Zg = int_params_Zg.mon_basis;
 
 %add intparams
-gH_Params = gen_grad_params(intParams_arr);
+g_h_params = gen_grad_params(int_params_arr);
 
-v_to_v_f = monomial_to_monomial(intParams_v.mon_basis, intParams_v_f.mon_basis);
-time_der_v = vector_derivative(1, intParams_v.mon_basis);
-mult_mat = vector_poly_multiply(f, intParams_v.mon_basis, intParams_v_f.mon_basis);
-space_der_v = vector_derivative(2, intParams_v.mon_basis);
+time_index = 1;
+first_sp_ind = 2;
 
-A1 = -intParams_v_f.mon_to_P0 * ((v_to_v_f * time_der_v) + (mult_mat * space_der_v)) * intParams_v.P0_to_mon;
-A2 = zeros(intParams_v_f.U, intParams_w.U);
-
-v_to_w_X_T = monomial_to_monomial(intParams_v.mon_basis, intParams_w_X_T.mon_basis);
-apply_final_time = vector_partial_application(1, T_max, intParams_v.mon_basis);
-
-B1 = intParams_w_X_T.mon_to_P0 * v_to_w_X_T * apply_final_time * intParams_v.P0_to_mon;
-B2 = zeros(intParams_w.U);
-
-v_to_w = monomial_to_monomial(intParams_v.mon_basis, intParams_w.mon_basis);
-apply_init_time = vector_partial_application(1, T_min, intParams_v.mon_basis);
-
-C1 = -intParams_w.mon_to_P0 * v_to_w * apply_init_time * intParams_v.P0_to_mon;
-C2 = eye(intParams_w.U);
-
-D1 = zeros(intParams_w.U, intParams_v.U);
-D2 = eye(intParams_w.U);
+L_f = liouville_operator(f, time_index, first_sp_ind, mon_basis_Z, mon_basis_Zf);
+V1 = int_params_Zf.mon_to_P0 * -L_f * int_params_Z.P0_to_mon;
+W1 = zero_operator(mon_basis_Y, mon_basis_Zf);
 
 
+%constraint 1
 
-const1 = zeros(intParams_v_f.U, 1);
-const2 = zeros(intParams_w_X_T.U, 1);
-const3 = ones(intParams_w.U, 1);
-const4 = zeros(intParams_w.U, 1);
+if error_dynamics
+    Z_to_Zf = monomial_to_monomial(mon_basis_Z, mon_basis_Zf);
+    Q1 = int_params_Zf.mon_to_P0 * -Z_to_Zf * int_params_Z.P0_to_mon;
+    prob_data.A = [V1, W1, Q1];
+else
+    prob_data.A = [V1, W1];
+end
 
-int1 = zeros(intParams_v.U, 1);
-int2 = intParams_w.w;
+c1 = zeros(size(mon_basis_Zf.monomials));
+prob_data.c = c1;
 
-probData.A = -[A1, A2;
-     B1, B2;
-     C1, C2;
-     D1, D2]';
-probData.c = -[const1;
-      const2;
-      const3;
-      const4];
-probData.b = -[int1;
-      int2];
+%constraint 2 (optional)
+
+if error_dynamics
+    L_g = liouville_operator(g, time_index, first_sp_index, mon_basis_Z, mon_basis_Zg);
+    V2 = int_params_Zg.mon_to_P0 * L_g * int_params_Z.P0_to_mon;
+    W2 = zero_operator(mon_basis_Y, mon_basis_Zg);
+
+    Z_to_Zg = monomial_to_monomial(mon_basis_Z, mon_basis_Zg);
+    Q2 = int_params_Zg.mon_to_P0 * Z_to_Zg * int_params_Z.P0_to_mon;
+    prob_data.A = [prob_data.A; 
+                   [V2, W2, Q2]];
+    c2 = zeros(size(mon_basis_Zg.monomials));
+    prob_data.c = [prob_data.c;
+                   c2];
+end
+
+%constraint 3 (optional)
+
+if error_dynamics
+    V3 = int_params_Zg.mon_to_P0 * -L_g * int_params_Z.P0_to_mon;
+    W3 = zero_operator(mon_basis_Y, mon_basis_Zg);
+    Z_to_Zg = monomial_to_monomial(mon_basis_Z, mon_basis_Zg);
+    Q3 = int_params_Zg.mon_to_P0 * Z_to_Zg * int_params_Z.P0_to_mon;
+    prob_data.A = [prob_data.A; 
+                   [V3, W3, Q3]];
+    c3 = zeros(size(mon_basis_Zg.monomials));
+    prob_data.c = [prob_data.c;
+                   c3];
+end
+
+%constraint 4 (optional)
+
+if error_dynamics
+    V4 = zero_operator(mon_basis_Z, mon_basis_Z);
+    W4 = zero_operator(mon_basis_Y, mon_basis_Z);
+    Q4 = eye(size(mon_basis_Z.monomials, 1));
+    c4 = zeros(size(mon_basis_Zg.monomials));
+    prob_data.A = [prob_data.A; 
+                   [V4, W4, Q4]];
+    prob_data.c = [prob_data.c;
+                   c4];
+end
+
+%constraint 5
+
+apply_init_time = vector_partial_application(time_index, T_min, mon_basis_Z);
+Z_to_Y0 = monomial_to_monomial(mon_basis_Z, mon_basis_Y0);
+V5 = int_params_Y0.mon_to_P0 * Z_to_Y0 * -apply_init_time * int_params_Z.P0_to_mon;
+W5 = zero_operator(mon_basis_Y, mon_basis_Y0);
+
+if error_dynamics
+    Q5 = zero_operator(mon_basis_Z, mon_basis_Y0);
+    prob_data.A = [prob_data.A;
+                   [V5, W5, Q5]];
+else
+    prob_data.A = [prob_data.A;
+                   [V5, W5]];
+end
+
+c5 = zeros(size(mon_basis_Y0.monomials));
+
+prob_data.c = [prob_data.c;
+               c5];
+
+%constraint 6
+
+V6 = zero_operator(mon_basis_Z, mon_basis_Y);
+W6 = eye(size(mon_basis_Y.monomials, 1));
+
+if error_dynamics
+    Q6 = zero_operator(mon_basis_Z, mon_basis_Y);
+    prob_data.A = [prob_data.A;
+                   [V6; W6; Q6]];
+else
+    prob_data.A = [prob_data.A;
+                   [V6, W6]];
+end
+
+c6 = zeros(size(mon_basis_Y.monomials));
+
+prob_data.c = [prob_data.c;
+               c6];
+
+%constraint 7
+
+Y_to_Z = monomial_to_monomial(mon_basis_Y, mon_basis_Z);
+
+V7 = eye(size(mon_basis_Z.monomials, 1));
+W7 = int_params_Z.mon_to_P0 * Y_to_Z * int_params_Y.P0_to_mon;
+
+if error_dynamics
+    Q7 = zero_operator(mon_basis_Z, mon_basis_Z);
+    prob_data.A = [prob_data.A;
+                   [V7, W7, Q7]];
+else
+    prob_data.A = [prob_data.A;
+                   [V7, W7]];
+end
+
+c7 = zeros(size(mon_basis_Z.monomials));
+
+prob_data.c = [prob_data.c;
+               c7];
+
+
+b1 = zeros(size(mon_basis_Z.monomials));
+prob_data.b = b1;
+
+b2 = -int_params_Y.w;
+prob_data.b = [prob_data.b;
+               b2];
+
+if error_dynamics
+    b3 = zeros(size(mon_basis_Z.monomials));
+    prob_data.b = [prob_data.b;
+                   b3];
+end
+
+% -Af + s = -c => Af >= c
+prob_data.A = -prob_data.A;
+prob_data.c = -prob_data.c;
 
 
 % make initial primal iterate
-x0 = ones(sum(gH_Params.U_arr), 1);
-[~, g0, ~, ~] = alfonso_grad_and_hess(x0, gH_Params);
-rP = max((1+abs(probData.b))./(1+abs(probData.A*x0)));
-rD = max((1+abs(g0))./(1+abs(probData.c)));
-x0 = repmat(sqrt(rP*rD),sum(gH_Params.U_arr),1);  
+x0 = ones(sum(g_h_params.U_arr), 1);
+[~, g0, ~, ~] = alfonso_grad_and_hess(x0, g_h_params);
+rP = max((1+abs(prob_data.b))./(1+abs(prob_data.A*x0)));
+rD = max((1+abs(g0))./(1+abs(prob_data.c)));
+x0 = repmat(sqrt(rP*rD),sum(g_h_params.U_arr),1);  
 
 % run alfonso
 opts.optimTol = 1e-6 ;
-results = alfonso(probData, x0, @alfonso_grad_and_hess, gH_Params, opts);
+results = alfonso(prob_data, x0, @alfonso_grad_and_hess, g_h_params, opts);
 
-hX = -(x-X_bounds(:, 1)).*(x-X_bounds(:, 2));
-hXT = -(x - X_T_bounds(1)).*(x-X_T_bounds(2));
-dl=boxMoments(x, X_bounds(:, 1), X_bounds(:, 2));
+alfonso_w_vec = int_params_Y.P0_to_mon * results.y(int_params_Z.U+1:int_params_Z.U+int_params_Y.U, 1);
+alfonso_v_vec = int_params_Z.P0_to_mon * results.y(1:int_params_Z.U, 1);
+w_alfonso = alfonso_w_vec' * int_params_Y.mon_basis.monomials;
+v_alfonso = alfonso_v_vec' * int_params_Z.mon_basis.monomials;
 
-[w_spotless, v_spotless] = liouvilleSolver(t, x, f, hX, hXT, dl, T_max, degree);
+if error_dynamics
+    alfonso_q_vec = int_params_Z.P0_to_mon * results.y(int_params_Z.U+int_params_Y.U+1:end, 1);
+    q_alfonso = alfonso_q_vec' * int_params_Z.mon_basis.monomials;
 
-%% PLOTTING POLYNOMIAL (RECOVERED) OUTPUT
+    out.q = q_alfonso;
+end
 
-spot_w_vec = msspoly_to_vector(w_spotless, intParams_w.mon_basis);
-alfonso_w_vec = intParams_w.P0_to_mon * results.y(intParams_v.U+1:end, 1);
-alfonso_v_vec = intParams_v.P0_to_mon * results.y(1:intParams_v.U, 1);
-w_alfonso = alfonso_w_vec' * intParams_w.mon_basis.monomials;
-v_alfonso = alfonso_v_vec' * intParams_v.mon_basis.monomials;
-norm_v_subs = norm(dmsubs(v_alfonso, variables, intParams_v.pts')' - results.y(1:intParams_v.U, 1))
-norm_v_f_subs = norm(intParams_v_f.mon_to_P0 * msspoly_to_vector(v_alfonso * f, intParams_v_f.mon_basis) - dmsubs(v_alfonso * f, variables, intParams_v_f.pts')')
-
-%PLOTTING ALL CONSTRAINTS
-
-v_to_v_f = monomial_to_monomial(intParams_v.mon_basis, intParams_v_f.mon_basis);
-time_der_v = vector_derivative(1, intParams_v.mon_basis);
-mult_mat = vector_poly_multiply(f, intParams_v.mon_basis, intParams_v_f.mon_basis);
-space_der_v = vector_derivative(2, intParams_v.mon_basis);
-
-
-constraint_1 = (intParams_v_f.P0_to_mon * A1 * results.y(1:intParams_v.U, 1))' * intParams_v_f.mon_basis.monomials;
-constraint_1_alt = (-(v_to_v_f * time_der_v + mult_mat * space_der_v) * intParams_v.P0_to_mon * results.y(1:intParams_v.U, 1))' * intParams_v_f.mon_basis.monomials;
-
-constraint_2 = (intParams_w_X_T.P0_to_mon * B1 *  results.y(1:intParams_v.U, 1))' * intParams_w.mon_basis.monomials;
-
-v_to_w = monomial_to_monomial(intParams_v.mon_basis, intParams_w.mon_basis);
-apply_init_time = vector_partial_application(1, T_min, intParams_v.mon_basis);
-
-constraint_3 = (intParams_w.P0_to_mon * C1 * results.y(1:intParams_v.U, 1) + alfonso_w_vec)' * intParams_w.mon_basis.monomials + msspoly(-1);
-
-constraint_4 = w_alfonso;
-
-close all
-
-
-figure('Name','v(T, x)') ; cla ; hold on ;
-xlim(X_T_bounds);
-ylim([-1, 1]);
-pts = linspace(X_T_bounds(1, 1), X_T_bounds(1, 2), 100);
-yvals = dmsubs(constraint_2, x, pts);
-plot(pts, yvals)
-plot(pts, zeros(size(pts)))
-
-figure('Name', '-v(0, x) + w(x) - 1') ; cla ; hold on ;
-xlim(X_bounds);
-ylim([-1, 1]);
-pts = linspace(X_bounds(1, 1), X_bounds(1, 2), 100);
-yvals = dmsubs(constraint_3, x, pts);
-plot(pts, yvals)
-plot(pts, zeros(size(pts)))
-
-figure('Name','w(x)') ; cla ; hold on ;
-xlim(X_bounds);
-ylim([-1, 1]);
-pts = linspace(X_bounds(1, 1), X_bounds(1, 2), 100);
-yvals = dmsubs(constraint_4, x, pts);
-plot(pts, yvals)
-plot(pts, zeros(size(pts)))
-
-int_spotless = def_int_on_box(w_spotless, X_bounds(:, 1), X_bounds(:, 2), variables)
-int_alfonso = def_int_on_box(w_alfonso, X_bounds(:, 1), X_bounds(:, 2), variables)
-
-figure('Name','-(\mathcal{L}_f v)(x)') ; cla ; hold on ;
-[grid_x, grid_y] = meshgrid(linspace(S_bounds(1, 1), S_bounds(1, 2), 50), linspace(S_bounds(2, 1), S_bounds(2, 2), 50));
-flat = [grid_x(:), grid_y(:)];
-const_1_vals = reshape(dmsubs(constraint_1, intParams_v_f.mon_basis.variables, flat')', 50, 50);
-const_1_alt_vals = reshape(dmsubs(constraint_1_alt, intParams_v_f.mon_basis.variables, flat')', 50, 50);
-
-COS(:,:,1) = zeros(50); % red
-COS(:,:,2) = zeros(50); % green
-COS(:,:,3) = zeros(50); % blue
-
-
-
-surf_a = surf(grid_x, grid_y, const_1_vals, COS, 'FaceAlpha',0.5);
-surf_c = surf(grid_x, grid_y, const_1_alt_vals, 'FaceAlpha',0);
-surf_c.EdgeColor="red";
-surf_b = surf(grid_x, grid_y, zeros(size(grid_x)), 'FaceAlpha',0);
-surf_b.EdgeColor = 'white';
-
-
-
-
-%[grid_x, grid_y] = meshgrid(linspace(-1, 1, 50), linspace(-1, 1, 50));
-%flat = [grid_x(:), grid_y(:)];
-x_grid = linspace(X_bounds(1, 1), X_bounds(1, 2), 50)';
-alf_vals = dmsubs(w_alfonso, intParams_w.mon_basis.variables, x_grid')';
-spot_vals = dmsubs(w_spotless, intParams_w.mon_basis.variables, x_grid')';
-
-
-
-figure('Name','w_s(x) and w_a(x)') ; cla ; hold on ;
-
-plot(x_grid, spot_vals,'LineWidth',1.5)
-plot(x_grid,alf_vals,'--', 'LineWidth',1);
+out.w = w_alfonso;
+out.v = v_alfonso;
 
 end
-%surf(grid_x, grid_y, alf_vals, COA);
-%surf(grid_x, grid_y, spot_vals, COS);
+
