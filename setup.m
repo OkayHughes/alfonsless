ab6d868383851ec3a3026b82f3d49d538b02ba2d
Owen Hughes <okayhughes@gmail.com>
2018-08-15 18:32:04 -0400

    Create support for multiple file systems (with a terrible hack)

--- setup.m
+++ setup.m
@@ -1,14 +1,1 @@
-addpath /Users/okayhughes/Documents/MATLAB/chebfun-master;
-addpath /Users/okayhughes/Documents/MATLAB/Padua2DM;
-addpath /Users/okayhughes/Documents/MATLAB/partitions;
-addpath /Users/okayhughes/Documents/MATLAB/spotless;
-addpath /Users/okayhughes/Documents/MATLAB/alfonso-master/
-addpath ./okay_poly_utils
-addpath ./spotless_utils
-addpath ./alfonso_utils
-addpath ./interpolant_bases
-addpath .
-cwd = pwd;
-cd /Users/okayhughes/Documents/MATLAB/spotless
-spot_install_lite
-cd(cwd);
+setup_okay();
