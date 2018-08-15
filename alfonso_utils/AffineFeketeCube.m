b80bcee651fbef0b5264c6036ea33d6aa73784c6
Owen Hughes <okayhughes@gmail.com>
2018-08-15 16:14:54 -0400

    Re-enable printing for AffineFeketeCube

--- alfonso_utils/AffineFeketeCube.m
+++ alfonso_utils/AffineFeketeCube.m
@@ -3,7 +3,7 @@ function affine_fekete_cube = AffineFeketeCube(variables, max_degree, box_scale)
     if check_for_basis(name, size(variables, 1), max_degree/2)
         fekete_cube = read_basis(name, size(variables, 1), max_degree/2);
     else
-        sprintf('Creating new %s with n=%d, d=%d', name, size(variables, 1), max_degree);
+        sprintf('Creating new %s with n=%d, d=%d', name, size(variables, 1), max_degree)
         fekete_cube = FeketeCube(size(variables, 1), max_degree/2);
         write_basis(fekete_cube);
     end
