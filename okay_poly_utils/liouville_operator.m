fa5f2686e9451405016733c1be576b42bf3ecaeb
Owen Hughes <okayhughes@gmail.com>
2018-08-14 20:40:47 -0400

    Refactor liouville_operator

--- okay_poly_utils/liouville_operator.m
+++ okay_poly_utils/liouville_operator.m
@@ -1,8 +1,8 @@
-function L_f = liouville_operator(f, time_index, first_sp_ind, last_sp_ind, in_basis, out_basis)
+function L_f = liouville_operator(f, time_index, first_sp_ind, in_basis, out_basis)
     v_to_vf = monomial_to_monomial(in_basis, out_basis);
     time_der = vector_derivative(time_index, in_basis);
     L_f = v_to_vf * time_der;
-    ct = 1
+    ct = 1;
     for var_ind=first_sp_ind:(first_sp_ind + size(f, 1) - 1)
         par_der = vector_derivative(var_ind, in_basis);
         mult_mat = vector_poly_multiply(f(ct), in_basis, out_basis);
