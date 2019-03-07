6c05f7bcf036d18aae5cfb6a0687c675336cf7b7
Owen Hughes <okayhughes@gmail.com>
2019-03-07 15:30:16 -0500

    Remove matlab-python interface files from master

--- test_segway.m
+++
@@ -1,8 +0,0 @@
-function res = test_segway(deg)
-	addpath('segway');
-	setup;
-	
-	res = segway_FRS_solver_okay(deg);
-end
-
-
--- try_exit.m
+++
@@ -1,9 +0,0 @@
-function try_exit(st)
-    try
-        run(st)
-    catch
-	"try_exit encountered error"
-        exit(1)
-    end
-    exit
-end
